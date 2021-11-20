1e169a85b7b5e4da660549436b0227388ee48555
DevankKH <94363581+DevankKH@users.noreply.github.com>
2021-11-20 13:46:02 +0200

    Update TestLaunch.h

--- TestLaunch.h
+++ TestLaunch.h
@@ -2,17 +2,17 @@
 #define TESTLAUNCH_H
 
 #include "Launch.h"
-#include "RocketLauncher.h"
+#include "Rocket.h"
 
 //ConcreteCommand class
 class TestLaunch : public Launch {
 	private:
 		//Rocket list to launch rockets - Means rocket class must have the 
 		//functions as they are receivers
-		RocketLauncher* rocket;
+		Rocket* rocket;
 	public:
 		//Initialise the variable to launch
-		TestLaunch(RocketLauncher*);
+		TestLaunch(Rocket*);
 		~TestLaunch();
 		virtual void execute();
 };
