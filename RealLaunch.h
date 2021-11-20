82d7647e266857a6b1085553672edb5306e6e54c
DevankKH <94363581+DevankKH@users.noreply.github.com>
2021-11-20 13:44:58 +0200

    Update RealLaunch.h

--- RealLaunch.h
+++ RealLaunch.h
@@ -2,17 +2,17 @@
 #define REALLAUNCH_H
 
 #include "Launch.h"
-#include "RocketLauncher.h"
+#include "Rocket.h"
 
 //ConcreteCommand class
 class RealLaunch : public Launch {
 	private:
 		//Rocket list to launch rockets - Means rocket class must have the 
 		//functions as they are receivers
-		RocketLauncher* rocket;
+		Rocket* rocket;
 	public:
 		//Initialise the variable to launch
-		RealLaunch(RocketLauncher*);
+		RealLaunch(Rocket*);
 		~RealLaunch();
 		virtual void execute();
 };
